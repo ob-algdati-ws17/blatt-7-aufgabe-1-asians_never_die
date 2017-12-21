06584c0b3fd369e3554184613d3f0b854b35b87d
LAPTOP-RFIJJ127\Duc <svsdlsg@web.de>
2017-12-21 22:33:29 +0100

    try

---
+++ Blatt7/test/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "list.cpp"
+
+using namespace std;
+
+int main() {
+    list *e1 = new list();
+    e1->insert(1,0);
+    cout << "use tests" << endl;
+    return 0;
+}
