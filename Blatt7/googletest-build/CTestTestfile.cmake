f8c7aae99aee576979ad5e4cbb943ebbe312df5a
Reawel <colberg@hm.edu>
2018-01-03 11:28:05 +0100

    initial commit

--- Blatt7/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /cygdrive/c/Users/Duc/CLionProjects/Praktikum1/blatt-4-aufgabe-1-Duciwuci/cmake-build-debug/googletest-src
-# Build directory: /cygdrive/c/Users/Duc/CLionProjects/Praktikum1/blatt-4-aufgabe-1-Duciwuci/cmake-build-debug/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
