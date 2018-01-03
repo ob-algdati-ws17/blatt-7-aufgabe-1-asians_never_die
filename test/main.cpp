2eda711b3febbcd81a916263c1b646c27f398fe3
Reawel <colberg@hm.edu>
2018-01-03 12:20:21 +0100

    Changed Test main.cpp

--- test/main.cpp
+++ test/main.cpp
@@ -1,11 +1,7 @@
 #include <iostream>
-#include "list.cpp"
 
 using namespace std;
 
 int main() {
-    list *e1 = new list();
-    e1->insert(1,0);
-    cout << "use tests" << endl;
     return 0;
 }
